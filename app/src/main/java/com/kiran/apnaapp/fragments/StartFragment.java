9ab38d4934757c1d1fc655d7b60e8fa212dc5e33
kiran thakur <kiran.zeroit@gmail.com>
2023-07-26 12:53:25 +0530

    27-july

---
+++ app/src/main/java/com/kiran/apnaapp/fragments/StartFragment.java
@@ -0,0 +1,25 @@
+package com.kiran.apnaapp.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.kiran.apnaapp.R;
+
+public class StartFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return getLayoutInflater().inflate(R.layout.fragment_start, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
