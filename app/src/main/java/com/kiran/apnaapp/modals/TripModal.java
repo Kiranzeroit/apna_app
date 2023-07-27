42e32541911f115d957bc0903300a7f83a70cd75
kiran thakur <kiran.zeroit@gmail.com>
2023-07-27 16:26:29 +0530

    set data in database

---
+++ app/src/main/java/com/kiran/apnaapp/modals/TripModal.java
@@ -0,0 +1,8 @@
+package com.kiran.apnaapp.modals;
+
+public class TripModal {
+    public String name= "";
+    public String budget= "";
+    public String startDate="";
+    public String endDate="";
+}
