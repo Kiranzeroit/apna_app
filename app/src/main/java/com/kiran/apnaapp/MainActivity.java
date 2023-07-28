d2c23dfef391c45c3912cf97ae1c56bf527f7431
karamjeet singh <karamjeet.zeroit@gmail.com>
2023-07-28 17:35:59 +0530

    implement circle image view and glide remove extra code create expanses adapter

--- app/src/main/java/com/kiran/apnaapp/MainActivity.java
+++ app/src/main/java/com/kiran/apnaapp/MainActivity.java
@@ -1,34 +1,19 @@
 package com.kiran.apnaapp;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationView;
-import com.kiran.apnaapp.activities.AddPlanActivity;
 import com.kiran.apnaapp.fragments.MenuFragment;
 import com.kiran.apnaapp.fragments.StartFragment;
 
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
     private FragmentManager fragmentManager;
-    private BottomNavigationView bottomNavigationView;
-    private Toolbar toolbar;
-    private DrawerLayout drawerLayout;
-    private ActionBarDrawerToggle actionBarDrawerToggle;
-    private NavigationView navigationView;
-    private ConstraintLayout constraintLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,24 +25,12 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     }
 
     private void initView() {
-        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
         bottomNavigationView.setSelectedItemId(R.id.entries);
 
-        /*drawerLayout = findViewById(R.id.drawerLayout);
-        navigationView = findViewById(R.id.navigationView);
-        constraintLayout = findViewById(R.id.clAdd);
-        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.nav_open, R.string.nav_close);
-        drawerLayout.addDrawerListener(actionBarDrawerToggle);
-        actionBarDrawerToggle.syncState();
-        constraintLayout.setOnClickListener(this);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);*/
-
     }
-
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -73,33 +46,4 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     private void loadFragment(Fragment fragment) {
         fragmentManager.beginTransaction().replace(R.id.frameLayout, fragment).commit();
     }
-
-
- /*   @Override
-    public void onBackPressed() {
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            drawerLayout.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }*/
-
- /*   public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-
-        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onClick(View view) {
-        if (view == constraintLayout) {
-            if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-                drawerLayout.closeDrawer(GravityCompat.START);
-            }
-            Intent intent = new Intent(MainActivity.this, AddPlanActivity.class);
-            startActivity(intent);
-        }
-    }*/
 }
