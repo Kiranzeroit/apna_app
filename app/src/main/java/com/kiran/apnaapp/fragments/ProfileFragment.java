5ff0303c0b92603656a587551260ffea633890cb
kiran thakur <kiran.zeroit@gmail.com>
2023-07-31 12:32:42 +0530

    get data from database and set in profile fragment

--- app/src/main/java/com/kiran/apnaapp/fragments/ProfileFragment.java
+++ app/src/main/java/com/kiran/apnaapp/fragments/ProfileFragment.java
@@ -1,5 +1,8 @@
 package com.kiran.apnaapp.fragments;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,14 +15,16 @@ import androidx.fragment.app.Fragment;
 
 import com.kiran.apnaapp.R;
 import com.kiran.apnaapp.Session;
+import com.kiran.apnaapp.activities.PlanDetailsActivity;
+import com.kiran.apnaapp.activities.SplashScreenActivity;
 import com.kiran.apnaapp.database.DatabaseHelper;
 import com.kiran.apnaapp.modals.DetailsModal;
 
 public class ProfileFragment extends Fragment implements View.OnClickListener {
     private AppCompatTextView tvName, tvCity, tvMobile, tvEmail, tvLogout;
-    private DatabaseHelper databaseHelper;
     private DetailsModal detailsModal;
     private Session session;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -29,10 +34,13 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        databaseHelper = new DatabaseHelper(requireActivity());
-        detailsModal= new DetailsModal();
-        session= new Session(requireActivity());
+        session = new Session(requireActivity());
+        DatabaseHelper databaseHelper = new DatabaseHelper(requireActivity());
+        detailsModal = new DetailsModal();
+        detailsModal = databaseHelper.getUserDetails(session.getStringValue("email"));
+
         initView(view);
+        setData();
     }
 
     private void initView(View view) {
@@ -42,10 +50,9 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
         tvEmail = view.findViewById(R.id.tvEmail);
         tvLogout = view.findViewById(R.id.tvLogout);
         tvLogout.setOnClickListener(this);
+    }
 
-
-        detailsModal= databaseHelper.getUserDetails(session.getStringValue("email"));
-
+    private void setData() {
         tvName.setText(detailsModal.name);
         tvCity.setText(detailsModal.city);
         tvEmail.setText(detailsModal.email);
@@ -54,9 +61,18 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
 
     @Override
     public void onClick(View view) {
-        if (view==tvLogout){
-
+        if (view == tvLogout) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+            builder.setTitle("Logout");
+            builder.setMessage("Are you sure you want to logout?");
+            builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    session.setBooleanValue("Login", false);
+                    startActivity( new Intent(requireActivity(), SplashScreenActivity.class));
+                }
+            });
+            builder.show();
         }
-
     }
 }
